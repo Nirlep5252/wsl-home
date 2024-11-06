cac4022b19194cc24440e54e01bbd3e32957943a
Nirlep5252 <hello@nirlep.dev>
2024-11-06 15:38:10 +0000

    chore: initial commit

---
+++ applications/shell.nix
@@ -0,0 +1,31 @@
+{ ... }: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "sudo"
+      ];
+    };
+    shellAliases = {
+      cd = "z";
+      n = "nvim";
+      hms = "home-manager switch --flake ~/wsl-home";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
---
+++ pkgs.nix
@@ -0,0 +1,21 @@
+{ pkgs, inputs, lib, ... }: {
+  home.packages = let
+    nvix = inputs.nvix.packages.${pkgs.system}.base.extend {
+      config.colorschemes.tokyonight.settings.transparent = false;
+      config.clipboard.providers.wl-copy.enable = lib.mkForce false;
+      config.clipboard.providers.xclip.enable = lib.mkForce true;
+      config.globals.copilot_browser = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    };
+  in [
+    # editors
+    nvix
+
+    # development
+    pkgs.lazydocker
+    pkgs.lazygit
+
+    # utilities
+    pkgs.wslu
+    pkgs.fastfetch
+  ];
+}
